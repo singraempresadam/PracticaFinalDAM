555a1ee1baeaa8e5e5c5c019aed25164d1055a69
Adrian Cardenal <SingraTura@users.noreply.github.com>
2019-05-30 21:10:41 +0200

    Inicio

---
+++ src/control/principal.java
@@ -0,0 +1,23 @@
+package control;
+
+import java.awt.EventQueue;
+
+import vista.ventana;
+
+public class principal {
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ventana frame = new ventana();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
---
+++ src/modelo/Persona.java
@@ -0,0 +1,5 @@
+package modelo;
+
+public abstract class Persona {
+	String nombre;
+}
