b1274887a11aea6ee4ce8de8b571bf3167aadf78
Adrian Cardenal <SingraTura@users.noreply.github.com>
2019-06-01 13:07:48 +0200

    update GestorModelo
    update darAltaMedico/Paciente/MedicoActivoNuevo para
    escribir en el archivo cada vez que se crea un nuevo objeto
    
    update modificadores de acceso obvios

--- src/modelo/GestorModelo.java
+++ src/modelo/GestorModelo.java
@@ -2,6 +2,8 @@ package modelo;
 
 import java.time.LocalTime;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.regex.Pattern;
 
 import lecturaYEscritura.DAO;
@@ -14,64 +16,74 @@ public class GestorModelo {
 	 * something, but if it´s of type T2, then do
 	 * something else," slap yourself
 	 * */
-	HashMap<String, Paciente> pacientes = new HashMap<String, Paciente>();
-	HashMap<String, Medico> medicos = new HashMap<String, Medico>();
-	HashMap<String, MedicoActivo> medicosActivo = new HashMap<String, MedicoActivo>();
-	HashMap<String, Cirujano> cirujanos = new HashMap<String, Cirujano>();
+	private HashMap<String, Paciente> pacientes = new HashMap<String, Paciente>();
+	private HashMap<String, Medico> medicos = new HashMap<String, Medico>();
+	private HashMap<String, MedicoActivo> medicosActivo = new HashMap<String, MedicoActivo>();
+	private HashMap<String, Cirujano> cirujanos = new HashMap<String, Cirujano>();
 
-	DTO <Paciente> dtoPaciente = new DTO<>("pacientes.dat");
-	DTO <Medico> dtoMedico = new DTO<>("medicos.dat");
-	DTO <MedicoActivo> dtoMedicoActivo = new DTO<>("medicosActivo.dat");
-	DTO <Cirujano> dtoCirujano = new DTO<>("cirujanos.dat");
+	private DTO <Paciente> dtoPaciente = new DTO<>("pacientes.dat");
+	private DTO <Medico> dtoMedico = new DTO<>("medicos.dat");
+	private DTO <MedicoActivo> dtoMedicoActivo = new DTO<>("medicosActivo.dat");
+	private DTO <Cirujano> dtoCirujano = new DTO<>("cirujanos.dat");
 	
-	DAO dao;
-	
+	public GestorModelo()
+	{
+		super();
+		this.cargarColeccionesMap();
+	}
 	public void darAltaPacienteNuevo(String nombre, String telefono, String direccion, String idUnico, String fechaDeNacimiento)
 	{
 		Paciente paciente = new Paciente(nombre, telefono, direccion, idUnico, fechaDeNacimiento);
 		pacientes.put(paciente.getIdUnico(), paciente);
-		//Escribir nueva lista pacientes
+		for (Entry<String, Paciente> pacienteLista : pacientes.entrySet()) {
+			dtoPaciente.grabar(pacienteLista.getValue());
+		}
 	}
-	public void darAltaMedico(String nombre, String telefono, String direccion, String idUnico, String especialidad)
+	public void darAltaMedicoNuevo(String nombre, String telefono, String direccion, String idUnico, String especialidad)
 	{
 		Medico medico= new Medico(nombre, telefono, direccion, idUnico, especialidad);
 		medicos.put(medico.getIdUnico(), medico);
-		//Escribir nueva lista medicos
+		for (Entry<String, Medico> medicoLista : medicos.entrySet()) {
+			dtoMedico.grabar(medicoLista.getValue());
+		}
 	}
-	public void darAltaMedicoActivo(String nombre, String telefono, String direccion, String idUnico, String especialidad, 
+	public void darAltaMedicoActivoNuevo(String nombre, String telefono, String direccion, String idUnico, String especialidad, 
 			LocalTime horaInicio, LocalTime horaFin, boolean [] dias, String consulta)
 	{
 		MedicoActivo medicoActivo = new MedicoActivo(nombre, telefono, direccion, idUnico, especialidad, horaInicio, horaFin ,dias, consulta);
 		medicosActivo.put(medicoActivo.getIdUnico(),medicoActivo);
+		for (Entry<String, MedicoActivo> medicoActivoLista : medicosActivo.entrySet()) {
+			dtoMedico.grabar(medicoActivoLista.getValue());
+		}
 	}
 	
-	public void cargarPaciente(Paciente pacienteLeido)
+	private void cargarPaciente(Paciente pacienteLeido)
 	{
 		Paciente paciente = new Paciente(pacienteLeido);
 		pacientes.put(paciente.getIdUnico(), paciente);
 	}
-	public void cargarMedico(Medico medicoLeido)
+	private void cargarMedico(Medico medicoLeido)
 	{
 		Medico medico = new Medico(medicoLeido);
 		medicos.put(medico.getIdUnico(), medico);
 	}
-	public void cargarMedicoActivo(MedicoActivo medicoActivoLeido)
+	private void cargarMedicoActivo(MedicoActivo medicoActivoLeido)
 	{
 		MedicoActivo medicoActivo = new MedicoActivo(medicoActivoLeido);
 		medicosActivo.put(medicoActivo.getIdUnico(), medicoActivo);
 	}
-	public void cargarCirujano(Cirujano cirujanoLeido)
+	private void cargarCirujano(Cirujano cirujanoLeido)
 	{
 		Cirujano cirujano = new Cirujano(cirujanoLeido);
 		cirujanos.put(cirujano.getIdUnico(), cirujano);
 	}
-	public void cargarColeccionesMap(){
+	private void cargarColeccionesMap(){
 		cargarColeccionMapPacientes();
 		cargarColeccionMapMedicos();
 		cargarColeccionMapMedicosActivo();
 		cargarColeccionMapCirujanos();
 	}
-	public void cargarColeccionMapPacientes()
+	private void cargarColeccionMapPacientes()
 	{
 		Paciente pacienteLeido;
 		do {
@@ -79,7 +91,7 @@ public class GestorModelo {
 			this.cargarPaciente(pacienteLeido);
 		} while (pacienteLeido != null);
 	}
-	public void cargarColeccionMapMedicos()
+	private void cargarColeccionMapMedicos()
 	{
 		Medico medicoLeido;
 		do {
@@ -87,7 +99,7 @@ public class GestorModelo {
 			this.cargarMedico(medicoLeido);
 		} while (medicoLeido != null);
 	}
-	public void cargarColeccionMapMedicosActivo()
+	private void cargarColeccionMapMedicosActivo()
 	{
 		MedicoActivo medicoActivoLeido;
 		do {
@@ -95,7 +107,7 @@ public class GestorModelo {
 			this.cargarMedicoActivo(medicoActivoLeido);
 		} while (medicoActivoLeido != null);
 	}
-	public void cargarColeccionMapCirujanos()
+	private void cargarColeccionMapCirujanos()
 	{
 		Cirujano cirujanoLeido;
 		do {
@@ -103,9 +115,5 @@ public class GestorModelo {
 			this.cargarCirujano(cirujanoLeido);
 		} while (cirujanoLeido != null);
 	}
-	
-	//personas.put(pepito.getIdUnico(), pepito);
-	//personas.get("123456789").getNombre());
-	
 	
 }
