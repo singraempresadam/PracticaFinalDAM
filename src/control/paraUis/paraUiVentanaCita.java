30e6f89d77302285e3039094c3a821de128e28ea
Adrian Cardenal <SingraTura@users.noreply.github.com>
2019-06-05 19:49:40 +0200

    add inicioParaUis

---
+++ src/control/paraUis/paraUiVentanaCita.java
@@ -0,0 +1,18 @@
+package control.paraUis;
+
+import vista.VentanaCita;
+
+public class paraUiVentanaCita extends VentanaCita{
+private Validator validator;
+	
+	public paraUiVentanaCita ()
+	{
+		this.validator = new Validator();
+		crearListeners();
+	}
+
+	private void crearListeners() {
+		
+		
+	}
+}
---
+++ src/control/paraUis/paraUiVentanaPrincipal.java
@@ -0,0 +1,20 @@
+package control.paraUis;
+
+import vista.ventana;
+
+public class paraUiVentanaPrincipal extends ventana{
+	private Validator validator;
+	
+	public paraUiVentanaPrincipal ()
+	{
+		this.validator = new Validator();
+		crearListeners();
+	}
+
+	private void crearListeners() {
+		
+		
+	}
+	
+	
+}
