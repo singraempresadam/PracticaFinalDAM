4e8366040dcc55b3e6b955bb3a7396e6863c41bb
Adrian Cardenal <SingraTura@users.noreply.github.com>
2019-06-07 08:39:25 +0200

    Fixed validator

--- src/control/paraUis/Validator.java
+++ src/control/paraUis/Validator.java
@@ -10,11 +10,16 @@ public class Validator{
 		String datosFragmentados[]= datosPaciente.split("-");
 		Respuesta retorno= new Respuesta(true,"No hay fallo");
 		Respuesta [] retornoArray= new Respuesta [5];
-		retornoArray[0]=Paciente.validaNombre(datosFragmentados[0]);
-		retornoArray[1]=Paciente.validaApellidos(datosFragmentados[1]);
-		retornoArray[2]=Paciente.validaTelefono(datosFragmentados[2]);
-		retornoArray[3]=Paciente.validaDireccion(datosFragmentados[3]);
-		retornoArray[4]=Paciente.validaFechaNacimiento(datosFragmentados[4]);
+		try {
+			retornoArray[0]=Paciente.validaNombre(datosFragmentados[0]);
+			retornoArray[1]=Paciente.validaApellidos(datosFragmentados[1]);
+			retornoArray[2]=Paciente.validaTelefono(datosFragmentados[2]);
+			retornoArray[3]=Paciente.validaDireccion(datosFragmentados[3]);
+			retornoArray[4]=Paciente.validaFechaNacimiento(datosFragmentados[4]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return new Respuesta(false,"Faltan parametros por introducir");
+		}
+		
 		int i=0;
 		while(retorno.isResultado() && i < retornoArray.length)
 		{
